fd8c72b41f9e6f4518dcec192ff495da6bfcf90d
Wanderson-Henrique-Oliveira <wandholiveira1@gmail.com>
2022-06-09 04:04:53 -0300

    Aprendendo JavaScript

---
+++ script.js
@@ -0,0 +1,18 @@
+let nome = 'Henrique';
+console.log(nome) //Está retornando um erro mas não consegui identificar o porque. Troquei de variável, de valor, o nome do dado e o erro persiste.
+
+let numero = 29;
+console.log(numero)
+
+let ano = 2022 - 29;
+console.log(ano)
+
+let soma = ano + numero;
+console.log(soma)
+
+let texto = "Estou mexendo no console.log";
+console.log(texto)
+
+const quartaFeira = false;
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
